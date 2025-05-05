06bea5796819bb4b34e5cd16bc688d332ba26619
BryanVenegas <20243rd060@utez.edu.mx>
2025-05-05 16:02:19 -0600

    mi edad

--- SayHello.java
+++ SayHello.java
@@ -2,7 +2,11 @@ public class SayHello{
     public static void main(String[] args) {
         System.out.println("hola ");
         System.out.println("bryan");
-        
+        System.out.println("22 años");
+
+
+
+
 
 
         
